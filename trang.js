a2b22f97caa56611f05cf2d9a5fb5bc9ddc58984
Tee <yen0948993473@gmail.com>
2024-11-08 17:41:32 +0700

    Add files via upload

--- trang.js
+++ trang.js
@@ -1,25 +1,57 @@
-// Lấy modal
-var modal = document.getElementById("myModal");
-
-// Lấy nút mở modal
-var btn = document.getElementById("openModalBtn");
-
-// Lấy phần tử <span> đóng modal
-var span = document.getElementsByClassName("close")[0];
-
-// Khi người dùng nhấn vào nút, mở modal
-btn.onclick = function() {
-    modal.style.display = "block";
+function changeImage(color) {
+    const ao = document.querySelectorAll('.ao');
+    
+    if (ao.length > 0) {
+        if (color === 'black') {
+            ao[0].src = "1344447_màu_đen-removebg-preview.png";
+        } else if (color === 'grey') {
+            ao[0].src = "1344447_màu_xám-removebg-preview.png";
+        }
+    }
 }
 
-// Khi người dùng nhấn vào <span> (x), đóng modal
-span.onclick = function() {
-    modal.style.display = "none";
+function changeImage1(color1) {
+    const ao1 = document.querySelectorAll('.ao1');
+    
+    if (ao1.length > 0) {
+        if (color1 === 'yellow') {
+            ao1[0].src = "image-removebg-preview (7).png";
+        } else if (color1 === 'brown') {
+            ao1[0].src = "image-removebg-preview (8).png";
+        } else if (color1 === 'black') {
+            ao1[0].src = "image-removebg-preview (9).png";
+        } else if (color1 === 'green') {
+            ao1[0].src = "7ccec356-zoro-removebg-preview.png";
+        }
+    }
 }
 
-// Khi người dùng nhấn vào bất cứ đâu bên ngoài modal, đóng modal
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+function changeImage2(color2) {
+    const ao2 = document.querySelectorAll('.ao2');
+    
+    if (ao2.length > 0) {
+        if (color2 === 'blue') {
+            ao2[0].src = "c3bff584-xanhduong-removebg-preview.png";
+        } else if (color2 === 'pink') {
+            ao2[0].src = "image-removebg-preview (1).png";
+        } else if (color2 === 'beige') {
+            ao2[0].src = "image-removebg-preview.png";
+        }
+    }
+}
+
+function changeImage4(color4) {
+    const ao4 = document.querySelectorAll('.ao4');
+    
+    if (ao4.length > 0) {
+        if (color4 === 'black') {
+            ao4[0].src = "image-removebg-preview (6).png";
+        } else if (color4 === 'grey') {
+            ao4[0].src = "image-removebg-preview (4).png";
+        } else if (color4 === 'beige') {
+            ao4[0].src = "image-removebg-preview (2).png";
+        } else if (color4 === 'white') {
+            ao4[0].src = "image-removebg-preview (5).png";
+        }
     }
 }
